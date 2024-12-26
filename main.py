a3b3b8ca4838d2b01ef0c98ad2c2ed2c683955a1
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-12-26 15:44:05 -0600

    Start

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 
+#This is the BudgetApp class. This will create the application and change the title to Budget App.
+class BudgetApp:  
+    def __init__(self,root):
+
+        root.title("Budget App")
+
+
 root = tk.Tk()
-
+BudgetApp(root)
 root.mainloop()
-
-print("peepoopperere")
-
