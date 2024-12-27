27301cf5a8c3fabb277a0a6bded6f47ee05f3940
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-12-26 19:40:23 -0600

    12/26 commit

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import tkinter as tk
+from tkinter import ttk
 
 #This is the BudgetApp class. This will create the application and change the title to Budget App.
 class BudgetApp:  
     def __init__(self,root):
 
+        #Name the window
         root.title("Budget App")
-
+        #Set size for window
+        root.geometry("1000x800")
+        
+        #This allows the user to change the size of the window
+        sizegrip= ttk.Sizegrip(root)
+        sizegrip.place(x=0, y=0, relx=1, rely=1, anchor="se")
 
 root = tk.Tk()
 BudgetApp(root)
