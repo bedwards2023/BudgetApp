d894b5d16c914018d75c7d7f1ede2fd21eeb137d
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-12-27 17:15:46 -0600

    Trying to get themes to work

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
-#This is the BudgetApp class. This will create the application and change the title to Budget App.
-class BudgetApp:  
-    def __init__(self,root):
 
-        #Name the window
-        root.title("Budget App")
+#This is the BudgetApp class. This will create the application and change the title to Budget App.
+class BudgetApp(tk.Tk):  
+    def __init__(self,title):
+
+        #main setup
+        super().__init__()
+        self.title("Budget App")
         #Set size for window
-        root.geometry("1000x800")
+        self.geometry("1000x800")
         
         #This allows the user to change the size of the window
-        sizegrip= ttk.Sizegrip(root)
-        sizegrip.place(x=0, y=0, relx=1, rely=1, anchor="se")
+        sizegrip= ttk.Sizegrip(self)
+        sizegrip.pack(side=tk.BOTTOM, anchor=tk.SE)
 
-root = tk.Tk()
-BudgetApp(root)
-root.mainloop()
+        #Theme
+        self.tk.call('source','elegance/elegance.tcl')
+        self.set_theme('light')
+
+if __name__ == '__main__':
+    BudgetApp('Budget App').mainloop()
+
